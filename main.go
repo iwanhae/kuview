8c71a384fdd5e687153e4c027a176788498add01
iwanhae <iwanhae@gmail.com>
2025-06-01 15:44:16 +0900

    Add event handling and JS integration for kuview

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"os"
+	"syscall/js"
 
 	"github.com/go-logr/logr"
 	"github.com/iwanhae/kuview/pkg/logger"
@@ -10,25 +13,39 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
+	log.Info().
+		Strs("env", os.Environ()).
+		Msg("Starting kuview")
+
+	if err := run(context.Background()); err != nil {
+		log.Fatal().Err(err).Msg("Failed to run kuview")
+	}
+}
+
+func run(ctx context.Context) error {
 	cfg := &rest.Config{
 		Host: os.Getenv("HOST"),
 	}
 
-	log.Info().
-		Strs("env", os.Environ()).
-		Msg("Starting kuview")
+	emitter, err := NewJSEventEmitter("kuview")
+	if err != nil {
+		return fmt.Errorf("failed to create event emitter: %w", err)
+	}
 
 	mgr, err := manager.New(cfg, manager.Options{
 		LeaderElection:   false,
@@ -37,7 +54,7 @@ func main() {
 		Logger:           logr.New(logger.New(log.Logger)),
 	})
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to create manager: %w", err)
 	}
 
 	for _, o := range []struct {
@@ -48,13 +65,84 @@ func main() {
 	} {
 		if err := builder.ControllerManagedBy(mgr).
 			Named("kuview").
-			Watches(o.obj, &handler.EnqueueRequestForObject{}).
+			WatchesMetadata(o.obj, &handler.EnqueueRequestForObject{}).
+			WithEventFilter(predicate.Funcs{
+				CreateFunc: func(e event.CreateEvent) bool {
+					emitter.Emit(&Event{
+						Type:   EventTypeCreate,
+						Object: e.Object,
+					})
+					return true
+				},
+				UpdateFunc: func(e event.UpdateEvent) bool {
+					emitter.Emit(&Event{
+						Type:   EventTypeUpdate,
+						Object: e.ObjectNew,
+					})
+					return true
+				},
+				DeleteFunc: func(e event.DeleteEvent) bool {
+					emitter.Emit(&Event{
+						Type:   EventTypeDelete,
+						Object: e.Object,
+					})
+					return true
+				},
+				GenericFunc: func(e event.GenericEvent) bool {
+					emitter.Emit(&Event{
+						Type:   EventTypeGeneric,
+						Object: e.Object,
+					})
+					return true
+				},
+			}).
 			Complete(o.ctr); err != nil {
-			panic(err)
+			return fmt.Errorf("failed to create controller: %w", err)
 		}
 	}
 
-	if err := mgr.Start(context.Background()); err != nil {
-		panic(err)
+	if err := mgr.Start(ctx); err != nil {
+		return fmt.Errorf("failed to start manager: %w", err)
 	}
+
+	return nil
+}
+
+type Event struct {
+	Type   EventType      `json:"type"`
+	Object runtime.Object `json:"object"`
+}
+
+type EventType string
+
+const (
+	EventTypeCreate  EventType = "create"
+	EventTypeUpdate  EventType = "update"
+	EventTypeDelete  EventType = "delete"
+	EventTypeGeneric EventType = "generic"
+)
+
+type EventEmitter struct {
+	document js.Value
+	Name     string
+}
+
+func NewJSEventEmitter(name string) (*EventEmitter, error) {
+	document := js.Global().Get("document")
+	res := EventEmitter{
+		Name:     name,
+		document: document,
+	}
+	return &res, nil
+}
+
+func (j *EventEmitter) Emit(v *Event) {
+	b, err := json.Marshal(v)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to marshal event")
+		return
+	}
+
+	val := js.Global().Get("JSON").Call("parse", string(b))
+	j.document.Call(j.Name, val)
 }
